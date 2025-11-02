9b601f5ad1381033112d98bec2b04ffc57625611
bsecurityenginer <benginer49@gmail.com>
2025-11-02 12:49:06 -0600

    rotate

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def main():
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.udate(dt)
                 
        screen.fill("black")
        player.draw(screen)
        pygame.display.flip()
 
        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,4 +1,6 @@
+import pygame
 from circleshape import *
+from constants import *
 
 class Player(CircleShape):
     def __init__(self,x,y)
@@ -16,4 +18,14 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.roatate(dt)
+
+
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
